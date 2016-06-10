fb713a5e1b98d0efee4e4eb3d3d6855251a137b5
dbaupload0031 <dba.upload0031@gmail.com>
2016-06-10 18:20:18 +0800

    Create Main002.scala
    apply archive

---
+++ Main002.scala
@@ -0,0 +1,11 @@
+import sys.process._
+import sam.src.db._
+
+package sam.exec.db
+{
+object Main002 {
+  def main(args: Array[String]) {
+  DBOperation.ApplyArchAuto(basis_info.logpath0)
+  }
+}
+}
